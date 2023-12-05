7ba3fd5314c346e30dfe45339d16469fd15d17d4
Nam Hoang <hoangnd@miamioh.edu>
2023-12-05 14:21:22 -0500

    Add result images

--- homework6.sh
+++ homework6.sh
@@ -7,9 +7,10 @@
 #SBATCH --time=1:00:00
 #SBATCH --mem=2GB
 #SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
 
 export OMP_NUM_THREADS=$SLURM_NTASKS
 
-/usr/bin/time -v ./homework1 images/Flag_of_the_US.png images/star_mask.png $RESULT true 50 32
+/usr/bin/time -v ./homework1 images/Flag_of_the_US.png images/star_mask.png results/image4.png true 50 32
 
 #end of script
